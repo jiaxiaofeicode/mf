38e163d1ffb0798f7329f6146ebc0a1f8d2df9bb
jiaxiaofei <jxf@qq.com>
2017-09-10 16:50:15 +0800

    change

--- demo.java
+++ demo.java
@@ -4,7 +4,7 @@ class Hello
 {
 	public static void main(String[] args) 
 	{
-System.out.println("123!");
-System.out.println("456!");
+System.out.println("ddsada!");
+System.out.println("ddsada!");
 	}
 }
