097ccdf59f1e542f109e38a05ad47c79c60bbaf2
jiaxiaofei <jxf@qq.com>
2017-09-28 11:41:47 +0800

    New Java File -Hello.java Create

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ class Hello
 {
 	public static void main(String[] args) 
 	{
-System.out.println("ddsada!");
-System.out.println("ddsada!");
+System.out.println("ddsadas!");
+System.out.println("ddsadas!");
 	}
 }
